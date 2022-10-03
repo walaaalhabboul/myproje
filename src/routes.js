87294c5cef5c97f8d105597345b6e447087c5f6d
walaaalhabboul <walaa.alhabboul@ogr.iuc.rdu.tr>
2022-10-03 15:43:12 +0300

    app updated

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,7 @@
 import HomePage from './components/HomePage.vue'
 import TabloB from './components/TabloB.vue'
-// import TabloS from './components/TabloS.vue'
 import ListS from './components/ListS.vue'
 import TaskCard from './components/TaskCard.vue'
-// import NavBr from './components/NavBr.vue'
 import ToDo from './components/ToDo.vue'
 import ToD from './components/ToD.vue'
 
@@ -12,11 +10,10 @@ const routes=[
 {path: '/Editor', component: HomePage ,name:"home"},
 {path: '/tablo', component: TabloB ,name:"tablo"},
 {path: '/list', component: ListS ,name:"ListS"},
-// {path: '/tablo1', component: TabloS ,name:"tablos"}
 {path: '/TaskCard', component: TaskCard ,name:"TaskCard"},
 {path: '/ToDo', component: ToDo ,name:"ToDo"},
 {path: '/ToD', component: ToD ,name:"ToD"},
-// {path: '/ToDo', component: NavBr ,name:"NavBr"},
+
 
 
 
