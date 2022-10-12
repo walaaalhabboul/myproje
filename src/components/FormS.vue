ae28680abf7f041bc6500b6b14613cfb6693aa38
walaaalhabboul <walaa.alhabboul@ogr.iuc.rdu.tr>
2022-10-12 12:12:35 +0300

    complete loading

--- src/components/FormS.vue
+++ src/components/FormS.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-form ref="form"   v-model="valid">
+  <v-form ref="form" v-model="valid">
     <v-col cols="12" sm="6">
       <v-text-field
         v-model="name"
@@ -13,22 +13,26 @@
       <v-text-field
         v-model="price"
         type="number"
-        :rules="[(v) => !!v || 'Price is required',
-    v => (v > 0) || 'price  must be more than 0 ']"
+        :rules="[
+          (v) => !!v || 'Price is required',
+          (v) => v > 0 || 'price  must be more than 0 ',
+        ]"
         label="Price"
         required
       ></v-text-field>
     </v-col>
     <p v-if="invalidinput">Nooo Data here ...you have to enter data</p>
     <v-row>
-        <v-col>
-            <v-btn class="mr-4" @click="submit" :disabled="!valid"> submit </v-btn>
+      <v-col>
+        <v-btn class="mr-4"  @click="submit" :disabled="!valid"> submit </v-btn>
+      </v-col>
 
-        </v-col>
+      <v-btn class="mr-4" @click="reset"> Reset Form </v-btn>
+     
+      <v-btn  class="mr-4" :loading="isloading" @click="loadData"> load data </v-btn>
 
-        <v-btn class="mr-4" @click="reset" > Reset Form </v-btn>
-    <v-btn class="mr-4" @click="loadData"> load data </v-btn>
-</v-row>
+    </v-row> 
+    <p>{{ responsdata[lastitem] }}</p>
   </v-form>
 </template>
 
@@ -41,6 +45,9 @@ export default {
       price: "",
       invalidinput: false,
       valid: true,
+      responsdata: [], 
+      lastitem: 0,
+      isloading:false
     };
   },
   methods: {
@@ -59,9 +66,13 @@ export default {
       console.log("Hi " + this.name);
     },
     loadData() {
+        this.isloading=true
       axios.get(`https://vuango-books-api.herokuapp.com/books/`).then((res) => {
-        console.log(res);
-      });
+        console.log(res.data);
+        if (res.data){ this.isloading=false}
+        this.responsdata = res.data;
+        this.lastitem = this.responsdata.length - 1;
+});
     },
     reset() {
       this.$refs.form.reset();
